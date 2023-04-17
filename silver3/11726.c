5b532e18c988724311c52ac36c11aa0c5c263e0e
jshwangbo <84737758+hbJeSeong@users.noreply.github.com>
2023-04-17 23:02:44 +0900

    Update 11726.c
    new source code

--- silver3/11726.c
+++ silver3/11726.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main() {
 	int input = 0, answer = 0, temp = 0;
-
 	scanf("%d", &input);
+	
 	for (int i = 0; (i * 5) <= input; i++)
 		for (int j = 0; (j * 3) <= input; j++) {
 			temp = i * 5 + j * 3;
